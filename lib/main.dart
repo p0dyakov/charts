6461588284fe7c4fa47803006d648cf3e8cc1c4d
Zahar Podyakov <usfuld@gmail.com>
2024-03-10 19:32:27 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MaterialApp(home: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final List<SalesData> chartData = [
+      SalesData(DateTime.parse('2011-12-01'), 35),
+      SalesData(DateTime.parse('2012-12-01'), 28),
+      SalesData(DateTime.parse('2013-12-01'), 34),
+      SalesData(DateTime.parse('2014-12-01'), 32),
+      SalesData(DateTime.parse('2015-12-01'), 40)
+    ];
+
     return Scaffold(
       body: Center(
         child: SfCartesianChart(
-          primaryXAxis: const CategoryAxis(),
-          series: <LineSeries<SalesData, String>>[
-            LineSeries<SalesData, String>(
-              dataSource: <SalesData>[
-                SalesData('Jan', 35),
-                SalesData('Feb', 28),
-                SalesData('Mar', 34),
-                SalesData('Apr', 32),
-                SalesData('May', 40)
-              ],
+          primaryXAxis: const DateTimeAxis(),
+          series: <CartesianSeries>[
+            // Renders line chart
+            LineSeries<SalesData, DateTime>(
+              dataSource: chartData,
               xValueMapper: (SalesData sales, _) => sales.year,
               yValueMapper: (SalesData sales, _) => sales.sales,
             )
@@ -34,6 +38,6 @@ class MainApp extends StatelessWidget {
 
 class SalesData {
   SalesData(this.year, this.sales);
-  final String year;
+  final DateTime year;
   final double sales;
 }
